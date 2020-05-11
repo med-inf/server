b82ddd3fb5988d157a607872d5ac1559707aaf3a
Michał Śledź <sledzson08@gmail.com>
2020-05-11 12:59:28 +0200

    Divide map into squares

--- src/main/java/pl/edu/agh/mi/server/data/Square.java
+++ src/main/java/pl/edu/agh/mi/server/data/Square.java
@@ -1,5 +1,10 @@
 package pl.edu.agh.mi.server.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class Square {
@@ -7,10 +12,27 @@ public class Square {
     private Position center;
     private double edgeLen;
 
+    @JsonIgnore
+    private final double top;
+    @JsonIgnore
+    private final double left;
+    @JsonIgnore
+    private final double bot;
+    @JsonIgnore
+    private final double right;
+
     public Square(Position center, double edgeLen) {
-        this.id = UUID.randomUUID();
+        this(UUID.randomUUID(), center, edgeLen);
+    }
+
+    public Square(UUID id, Position center, double edgeLen) {
+        this.id = id;
         this.center = center;
         this.edgeLen = edgeLen;
+        top = center.getLat() + 0.5 * edgeLen;
+        left = center.getLon() - 0.5 * edgeLen;
+        bot = center.getLat() - 0.5 * edgeLen;
+        right = center.getLon() + 0.5 * edgeLen;
     }
 
     public UUID getId() {
@@ -23,5 +45,11 @@ public class Square {
 
     public double getEdgeLen() {
         return edgeLen;
+    }
+
+    public boolean isInSquare(Position position) {
+        double lat = position.getLat();
+        double lon = position.getLon();
+        return  top >= lat && lat >= bot && right >= lon && lon >= left;
     }
 }

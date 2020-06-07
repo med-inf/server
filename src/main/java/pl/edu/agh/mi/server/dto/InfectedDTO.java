75c67f4bfcd90a30a652b291b12e9ffdb61b9a75
Michał Śledź <sledzson08@gmail.com>
2020-06-07 13:25:09 +0200

    Add admin panel

---
+++ src/main/java/pl/edu/agh/mi/server/dto/InfectedDTO.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.mi.server.dto;
+
+public class InfectedDTO {
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
